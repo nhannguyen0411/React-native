4bbfe93c232d9b3b772c5f34b8e7a73823f8363c
Kibuto <nguyentrongnhanc8@gmail.com>
2020-01-07 00:06:01 +0700

    add screen login + feature

--- AppNavigator.js
+++ AppNavigator.js
@@ -3,10 +3,17 @@ import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import ScreenHome from './Screen/ScreenHome';
 import ScreenAbout from './Screen/ScreenAbout';
+import ScreenLogin from './Screen/ScreenLogin';
 
-const AppNavigator = createStackNavigator({
+const AppNavigator = createStackNavigator(
+  {
   Home: {screen: ScreenHome},
-  About: {screen: ScreenAbout}
-});
+  About: {screen: ScreenAbout},
+  Login: ScreenLogin
+  },
+  {
+    initialRouteName: 'Login'
+  }
+);
 
 export default AppNavigator;
